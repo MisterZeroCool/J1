86b0c773b3537701f649626836c07859a95c760e
Fox <gir_for@mail.ru>
2022-06-02 15:08:47 +0300

    HomeWork_e Работа над ошибками в домашней работе homework_e. Учел свои ошибки и улучшил его. Перенес все лишнее в родительский класс, избежал дублирования кода

---
+++ src/ru/java_one/lesson_e/homework_e/home_work_on_mistakes/Dog.java
@@ -0,0 +1,9 @@
+package ru.java_one.lesson_e.homework_e.home_work_on_mistakes;
+
+class Dog extends Animal{
+
+    Dog(String name, float maxJump, float maxRun, float maxSwim) {
+        super("Dog", name, maxJump, maxRun, maxSwim);
+    }
+
+}
